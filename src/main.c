c73cb0035a3c9975b2c1a09cc312db3ec71f54e7
SongZihui-sudo <1751122876@qq.com>
2024-08-05 23:58:39 +0800

    add: read Shutterspeed, iso, Aperture

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "oled/Pico_OLED_0in96.h"
 
 static GPPortInfoList* portinfolist   = NULL;
 static CameraAbilitiesList* abilities = NULL;
@@ -52,6 +53,108 @@ static GPContext* sample_create_context( )
     return context;
 }
 
+/*
+ * This function looks up a label or key entry of
+ * a configuration widget.
+ * The functions descend recursively, so you can just
+ * specify the last component.
+ */
+
+static int _lookup_widget( CameraWidget* widget, const char* key, CameraWidget** child )
+{
+    int ret;
+    ret = gp_widget_get_child_by_name( widget, key, child );
+    if ( ret < GP_OK )
+        ret = gp_widget_get_child_by_label( widget, key, child );
+    return ret;
+}
+
+/* Gets a string configuration value.
+ * This can be:
+ *  - A Text widget
+ *  - The current selection of a Radio Button choice
+ *  - The current selection of a Menu choice
+ *
+ * Sample (for Canons eg):
+ *   get_config_value_string (camera, "owner", &ownerstr, context);
+ */
+static int get_config_value_string( Camera* camera, const char* key, char** str, GPContext* context )
+{
+    CameraWidget *widget = NULL, *child = NULL;
+    CameraWidgetType type;
+    int ret;
+    char* val;
+
+    ret = gp_camera_get_config( camera, &widget, context );
+    if ( ret < GP_OK )
+    {
+        fprintf( stderr, "camera_get_config failed: %d\n", ret );
+        return ret;
+    }
+    ret = _lookup_widget( widget, key, &child );
+    if ( ret < GP_OK )
+    {
+        fprintf( stderr, "lookup widget failed: %d\n", ret );
+        goto out;
+    }
+
+    /* This type check is optional, if you know what type the label
+     * has already. If you are not sure, better check. */
+    ret = gp_widget_get_type( child, &type );
+    if ( ret < GP_OK )
+    {
+        fprintf( stderr, "widget get type failed: %d\n", ret );
+        goto out;
+    }
+    switch ( type )
+    {
+        case GP_WIDGET_MENU:
+        case GP_WIDGET_RADIO:
+        case GP_WIDGET_TEXT:
+            break;
+        default:
+            fprintf( stderr, "widget has bad type %d\n", type );
+            ret = GP_ERROR_BAD_PARAMETERS;
+            goto out;
+    }
+
+    /* This is the actual query call. Note that we just
+     * a pointer reference to the string, not a copy... */
+    ret = gp_widget_get_value( child, &val );
+    if ( ret < GP_OK )
+    {
+        fprintf( stderr, "could not query widget value: %d\n", ret );
+        goto out;
+    }
+    /* Create a new copy for our caller. */
+    *str = strdup( val );
+out:
+    gp_widget_free( widget );
+    return ret;
+}
+
+int init_0in96_olcd( ) 
+{
+    if ( DEV_ModuleInit( ) != 0 )
+    {
+        fprintf( stderr, "0.96 OLED init fail: %d\n", -1 );
+        return -1;
+    }
+
+    fprintf( stdout, "OLED Init...\r\n" );
+    OLED_Init( );
+    OLED_Clear( );
+
+    while (1) 
+    {
+        OLED_ShowString( 0, 0, "luckfox", 16 );
+        DEV_Delay_ms( 1000 );
+        OLED_Clear( );
+    }
+
+    return 0;
+}
+
 int main( int argc, char** argv )
 {
     Camera* camera;
@@ -59,6 +162,9 @@ int main( int argc, char** argv )
     char* owner;
     GPContext* context;
     CameraText text;
+    void* evtdata;
+
+    init_0in96_olcd( );
 
     context = sample_create_context( ); /* see context.c */
     gp_camera_new( &camera );
@@ -86,9 +192,51 @@ int main( int argc, char** argv )
     }
     printf( "Summary:\n%s\n", text.text );
 
+    // get shutter speed
+    char* val;
+    float shutterspeed;
+    ret = get_config_value_string( camera, "shutterspeed", &val, context );
+    if ( ret == GP_OK )
+    {
+        if ( strchr( val, '/' ) )
+        {
+            int zaehler, nenner;
+            sscanf( val, "%d/%d", &zaehler, &nenner );
+            shutterspeed = 1.0 * zaehler / nenner;
+        }
+        else
+        {
+            if ( !sscanf( val, "%g", &shutterspeed ) )
+                shutterspeed = 0.0;
+        }
+        int res_shutterspeed = 1 / shutterspeed;
+        printf( "Shutterspeed is 1 / %d (%g) \n", res_shutterspeed, shutterspeed );
+        free( val );
+    }
+
+    // get iso
+    float iso = 0.0;
+    ret = get_config_value_string( camera, "iso", &val, context );
+    if ( ret == GP_OK )
+    {
+        sscanf( val, "%g", &iso );
+        printf( "ISO is %g\n", iso );
+        free( val );
+    }
+
+    // get aperture
+    float aperture = 0;
+    ret = get_config_value_string( camera, "f-number", &val, context );
+    if ( ret == GP_OK )
+    {
+        sscanf( val, "f/%g", &aperture );
+        printf( "Aperture is %s (%g)\n", val, aperture );
+        free( val );
+    }
+
     gp_camera_exit( camera, context );
     gp_camera_free( camera );
     gp_context_unref( context );
-    return 0;
+
     return 0;
 }
